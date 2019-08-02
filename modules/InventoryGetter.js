337eb4f36c4c7f2c8a6997001af93fcabedd741a
briansd9 <briansd9@binkmail.com>
2019-08-02 23:16:48 +0800

    fixed: don't count equipment taken off as a map drop

--- modules/InventoryGetter.js
+++ modules/InventoryGetter.js
@@ -175,6 +175,7 @@ class InventoryGetter extends EventEmitter {
       if (item.inventoryId === "MainInventory") {
         mainInventory[item.id] = item;
       } else {
+        mainInventory[item.id] = item;
         equippedItems[item.id] = item;
       }
     });
