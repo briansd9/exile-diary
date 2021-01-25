53d02e98a313014c8ae3cd84bcff58252e4a24b5
briansd9 <10414098+briansd9@users.noreply.github.com>
2021-01-25 23:02:35 +0800

    added data fixing functions for new league bugs

--- res/debug.js
+++ res/debug.js
@@ -16,7 +16,10 @@ function recheckGained() {
   
   $(".debugButton").prop("disabled", true);
   $("#debugOutput").html("");
-  RunParser.recheckGained().then(() => {
+  
+  let startDate = $("#recheckGainedStartValue").val().substring(0, 8) + "000000";
+  
+  RunParser.recheckGained(startDate).then(() => {
     $(".debugButton").prop("disabled", false);
     $("#debugOutput").append(`Done.`);
   });
@@ -99,7 +102,6 @@ function checkGearDuplicates() {
   
 }
 
-
 async function migrateAll() {
   
   $(".debugButton").prop("disabled", true);
@@ -123,8 +125,8 @@ async function migrateAll() {
   
 }
 
-function execSql() {
-  let sql = $("#debugOutput").val();
+function execSql(sql) {
+  sql = sql || $("#debugOutput").val();
   let DB = require('./modules/DB').getDB();
   DB.run(sql, [], function(err) {
     $("#debugsql").hide();
@@ -136,6 +138,226 @@ function execSql() {
       $("#debugOutput").val(`Records changed: ${this.changes}.`);
     }
   });
+}
+
+async function v0286fix() {
+  
+  $("#debugOutput").html("");
+  $(".debugButton").prop("disabled", true);
+  
+  let DB = require('./modules/DB').getDB();  
+  debugLog("Starting...\n");
+  
+  await devalueRogueMarkers();
+  await fixConquerorBattleCounts();
+  await fixZanaBlightedMaps();
+  await getMavenEvents();
+  
+  $(".debugButton").prop("disabled", false);
+  debugLog("Done.");
+  
+  async function devalueRogueMarkers() {
+    debugLog("Setting value of Rogue's Marker drops to zero...");
+    return new Promise( async (resolve, reject) => {
+      DB.run(" update items set value = 0 where typeline = 'Rogue''s Marker' and value > 0 ", function(err) {
+        if(err) {
+          debugLog(err.toString());
+        } else {
+          debugLog(`${this.changes} records changed.\n`);
+        }
+        resolve(1);
+      });
+    });
+  }
+  
+  async function fixConquerorBattleCounts() {
+
+    debugLog("Fixing Conqueror and Sirus battle counts...");
+    
+    return new Promise( async (resolve, reject) => {
+      
+      DB.run("begin transaction");
+      let conqs = require('./modules/Constants').conquerors;
+      let runs = await getMapsSql(" select * from mapruns where id > 20210115000000 and runinfo like '%conq%' and runinfo like '%sirus%' ");
+      let updateStmt = DB.prepare(" update mapruns set runinfo = ? where id = ? ");
+      
+      for(let i = 0; i < runs.length; i++) {
+        let run = runs[i];
+        let obj = JSON.parse(run.runinfo);
+        delete obj.sirusBattle;
+        conqs.forEach(c => {
+          if(obj.conqueror[c]) {
+            obj.conqueror[c].encountered = true;
+            delete(obj.conqueror[c].battle);
+          }
+        });
+        let str = JSON.stringify(obj);
+        let d = await execStmt(updateStmt, [str, run.id]);
+        if(d) {
+          debugLog(`Fixed conqueror battle count for map ${i+1} of ${runs.length} with id ${run.id}.`);
+        } else {
+          debugLog(`Error conqueror battle count for map ${i+1} of ${runs.length} with id ${run.id}.`);
+        }
+      }
+      
+      if(runs.length === 0) {
+        debugLog(`Didn't find any that needed fixing.\n`);
+      }
+      
+      DB.run("commit");
+      resolve(1);
+      
+    });
+    
+  }
+  
+  async function fixZanaBlightedMaps() {
+
+    debugLog("Fixing Zana blighted map missions...");
+    
+    return new Promise( async (resolve, reject) => {
+      
+      DB.run("begin transaction");
+      let runs = await getMapsSql(" select * from mapruns where id > 20210115000000 and runinfo like '%blightedMap%' and runinfo like '%Zana%' ");
+      let updateStmt = DB.prepare(" update mapruns set runinfo = ? where id = ? ");
+      
+      for(let i = 0; i < runs.length; i++) {
+        let run = runs[i];
+        let obj = JSON.parse(run.runinfo);
+        delete obj.blightedMap;
+        obj.masters["Zana, Master Cartographer"].blightedMissionMap = true;
+        let str = JSON.stringify(obj);
+        let d = await execStmt(updateStmt, [str, run.id]);
+        if(d) {
+          debugLog(`Fixed Zana blighted mission for map ${i+1} of ${runs.length} with id ${run.id}.`);
+        } else {
+          debugLog(`Error fixing Zana blighted mission for map ${i+1} of ${runs.length} with id ${run.id}.`);
+        }
+      }
+      
+      if(runs.length === 0) {
+        debugLog(`Didn't find any that needed fixing.\n`);
+      }
+      
+      DB.run("commit");
+      resolve(1);
+      
+    });
+    
+  }
+  
+  async function getMavenEvents() {
+    
+    debugLog("Updating maps with Maven events...");
+    
+    return new Promise( async (resolve, reject) => {
+      
+      DB.run("begin transaction");
+      let mavenQuotes = require('./modules/Constants').mavenQuotes;
+      let Utils  = require('./modules/Utils');
+      let runs = await getMapsSql(`
+        select * from mapruns where id > 20210115000000
+        and exists(select 1 from events where id between mapruns.firstevent and mapruns.lastevent and event_text like 'The Maven%');
+      `);
+      let updateStmt = DB.prepare(" update mapruns set runinfo = ? where id = ? ");
+      
+      for(let j = 0; j < runs.length; j++) {
+        let run = runs[j];
+        let obj = JSON.parse(run.runinfo);
+        let mapEvents = await getMavenMapEvents(run.firstevent, run.lastevent);
+        for(let i = 0; i < mapEvents.length; i++) {
+          if(!obj.maven) {
+            obj.maven = { firstLine : mapEvents[i].id };
+          } else {
+            let eventType = mavenQuotes[mapEvents[i].text];
+            if(eventType) {
+              obj.maven[eventType] = mapEvents[i].id;
+            }
+          }
+        }
+        if(obj.maven) {
+          if(obj.maven.bossKilled) {
+            obj.bossBattle = {};
+            obj.bossBattle.time = Utils.getRunningTime(obj.maven.firstLine, obj.maven.bossKilled, "s", { useGrouping : false } );
+            let deaths = await getBossBattleDeaths(obj.maven.firstLine, obj.maven.bossKilled);
+            if(deaths) {
+              obj.bossBattle.deaths = deaths;
+            }
+          }
+          let str = JSON.stringify(obj);
+          let d = await execStmt(updateStmt, [str, run.id]);
+          if(d) {
+            debugLog(`Added Maven events for map ${j+1} of ${runs.length} with id ${run.id} ${obj.bossBattle ? ` - boss killed in ${obj.bossBattle.time} sec ${ obj.bossBattle.deaths ? `- ${obj.bossBattle.deaths} deaths` : ""}` : ""}`);
+          } else {
+            debugLog(`Error adding Maven events for map with id ${run.id}`);
+          }
+        }
+      }
+      
+      DB.run("commit");
+      resolve(1);
+      
+    });
+    
+    function getBossBattleDeaths(start, end) {
+      return new Promise( (resolve, reject) => {
+        DB.get(`select count(1) as count from events where event_type='slain' and id between ? and ? order by id`, [start, end], async (err, row) => {        
+          if(err) {
+            debugLog(err.toString());
+            resolve(0);
+          } else {
+            resolve(row.count);
+          }
+        });
+      });
+    }
+    
+    function getMavenMapEvents(firstevent, lastevent) {
+      return new Promise( (resolve, reject) => {
+        DB.all(`
+          select id, substr(event_text, 12) as text from events 
+          where event_type='leagueNPC' and event_text like 'The Maven:%' 
+          and id between ? and ? order by id
+        `, [firstevent, lastevent], async (err, evts) => {        
+          if(err) {
+            debugLog(err.toString());
+            resolve([]);
+          } else {
+            resolve(evts);
+          }
+        });
+      });
+    }
+    
+  }
+  
+  function execStmt(stmt, data) {
+    return new Promise( (resolve, reject) => {
+      stmt.run(data, async (err) => {
+        if(err) {
+          debugLog(`Error executing statement: ${err}`);
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      });
+    });       
+  }
+  
+  function getMapsSql(sql) {
+    return new Promise( (resolve, reject) => {
+      DB.all(sql, (err, runs) => {
+        if(err) {
+          debugLog(err.toString());
+          resolve([]);
+        } else {
+          resolve(runs);
+        }
+      });
+    });
+  }
+  
+  
 }
 
 
@@ -186,7 +408,7 @@ async function migrateLeagueDBData(char, force = false) {
             debugLog(`Inserting ${leagues[i].league} stashes`);
             for(let j = 0; j < stashes.length; j++) {
               let s = stashes[j];
-              let d = await insertData(stashStmt, [s.timestamp, s.items, s.value]);
+              let d = await execStmt(stashStmt, [s.timestamp, s.items, s.value]);
               if(d) {
                 debugLog(`Inserted stash ${s.timestamp} with value ${s.value}`);
               } else {
@@ -203,7 +425,7 @@ async function migrateLeagueDBData(char, force = false) {
             debugLog(`Inserting ${leagues[i].league} rates`);
             for(let j = 0; j < rates.length; j++) {
               let s = rates[j];
-              let d = await insertData(ratesStmt, [s.date, s.data]);
+              let d = await execStmt(ratesStmt, [s.date, s.data]);
               if(d) {
                 debugLog(`Inserted rates for ${s.date}`);
               } else {
@@ -213,7 +435,7 @@ async function migrateLeagueDBData(char, force = false) {
           }
 
           let charStmt = leagueDB.prepare(" insert into characters values(?)");
-          let d = await insertData(charStmt, [ char ]);
+          let d = await execStmt(charStmt, [ char ]);
           if(d) {
             debugLog(`Inserted charName ${char}`);
           } else {
@@ -311,11 +533,11 @@ async function migrateLeagueDBData(char, force = false) {
     });       
   }
 
-  async function insertData(stmt, data) {
+  async function execStmt(stmt, data) {
     return new Promise( (resolve, reject) => {
       stmt.run(data, async (err) => {
         if(err) {
-          debugLog(`Error inserting data: ${err}`);
+          debugLog(`Error executing statement: ${err}`);
           resolve(false);
         } else {
           resolve(true);
